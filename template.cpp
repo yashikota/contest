634d8d99b88afcd95520e71602cfba46b7c59e3e
kota <52403688+yashikota@users.noreply.github.com>
2022-08-02 00:51:17 +0900

    first commit

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+int main()
+{
+    int answer = 0;
+
+    cout << answer << endl;
+
+    return 0;
+}
