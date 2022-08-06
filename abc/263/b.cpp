fdf269a76aef818fc7311e6eb60dd7dafd4d72d6
kota <52403688+yashikota@users.noreply.github.com>
2022-08-06 22:37:13 +0900

    add: abc263

---
+++ abc/263/b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+int main()
+{
+    int N;
+    int P1;
+    cin >> N >> P1;
+
+    cout << N - P1;
+
+    return 0;
+}
