5053672c0a2c39eda84393bea1ef4996ea37ea48
kota <52403688+yashikota@users.noreply.github.com>
2023-01-28 14:49:33 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -3,8 +3,7 @@ using namespace std;
 // #include <atcoder/all>
 // using namespace atcoder;
 
-void yes(int type);
-void no(int type);
+void result(int letter, int either);
 
 int main()
 {
@@ -16,40 +15,23 @@ int main()
     return 0;
 }
 
-void yes(int type)
+void result(int letter, int either)
 {
     string output;
 
-    switch (type)
+    switch (letter)
     {
     case 1:
-        output = "YES";
+        if (either) output = "YES";
+        else output = "NO";
         break;
     case 2:
-        output = "Yes";
+        if (either) output = "Yes";
+        else output = "No";
         break;
     case 3:
-        output = "yes";
-        break;
-    }
-
-    cout << output << endl;
-}
-
-void no(int type)
-{
-    string output;
-
-    switch (type)
-    {
-    case 1:
-        output = "NO";
-        break;
-    case 2:
-        output = "No";
-        break;
-    case 3:
-        output = "no";
+        if (either) output = "yes";
+        else output = "no";
         break;
     }
 
