050589c89bb89558d32f525ce22f22ce9c0dcb84
kota <52403688+yashikota@users.noreply.github.com>
2023-02-20 10:21:49 +0900

    fmt template

--- template.cpp
+++ template.cpp
@@ -7,35 +7,38 @@ using namespace std;
 
 void result(int letter, int either);
 
-int main()
-{
-    string str;
+int main() {
+  string str;
 
-    cin >> str;
-    cout << str << endl;
+  cin >> str;
+  cout << str << endl;
 
-    return 0;
+  return 0;
 }
 
-void result(int letter, int either)
-{
-    string output;
+void result(int letter, int either) {
+  string output;
 
-    switch (letter)
-    {
+  switch (letter) {
     case 1:
-        if (either) output = "YES";
-        else output = "NO";
-        break;
+      if (either)
+        output = "YES";
+      else
+        output = "NO";
+      break;
     case 2:
-        if (either) output = "Yes";
-        else output = "No";
-        break;
+      if (either)
+        output = "Yes";
+      else
+        output = "No";
+      break;
     case 3:
-        if (either) output = "yes";
-        else output = "no";
-        break;
-    }
+      if (either)
+        output = "yes";
+      else
+        output = "no";
+      break;
+  }
 
-    cout << output << endl;
+  cout << output << endl;
 }
