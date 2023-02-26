8eb56c92cfca9a9d280c21a647af8e08be347280
kota <52403688+yashikota@users.noreply.github.com>
2023-02-27 01:51:52 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -1,18 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-// #include <atcoder/all>
-// using namespace atcoder;
 
-#define YES result(1, 1)
-#define Yes result(2, 1)
-#define yes result(3, 1)
-#define NO result(1, 0)
-#define No result(2, 0)
-#define no result(3, 0)
+#define YES cout << "YES" << endl;
+#define Yes cout << "Yes" << endl;
+#define yes cout << "yes" << endl;
+#define NO cout << "NO" << endl;
+#define No cout << "No" << endl;
+#define no cout << "no" << endl;
 
 #define rep(i, n) for (int i = 0; i < (n); i++)
-
-void result(int letter, int either);
 
 int main() {
     int a;
@@ -22,29 +18,3 @@ int main() {
     return 0;
 }
 
-void result(int letter, int either) {
-    string output;
-
-    switch (letter) {
-        case 1:
-            if (either)
-                output = "YES";
-            else
-                output = "NO";
-            break;
-        case 2:
-            if (either)
-                output = "Yes";
-            else
-                output = "No";
-            break;
-        case 3:
-            if (either)
-                output = "yes";
-            else
-                output = "no";
-            break;
-    }
-
-    cout << output << endl;
-}
