5a954498fd1ad87511a7b85caddd98c7ba65f538
kota <52403688+yashikota@users.noreply.github.com>
2023-03-12 07:38:58 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -1,13 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define YES cout << "YES" << endl;
-#define Yes cout << "Yes" << endl;
-#define yes cout << "yes" << endl;
-#define NO cout << "NO" << endl;
-#define No cout << "No" << endl;
-#define no cout << "no" << endl;
-
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
 int main() {
