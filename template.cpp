1eb2bce00e32fbeba8fffd265cd5331d4d321c99
kota <52403688+yashikota@users.noreply.github.com>
2023-08-10 21:21:08 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -1,17 +1,9 @@
 #include <bits/stdc++.h>
-using namespace std;
 
 #define ll long long
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
 int main() {
-    int n;
-    cin >> n;
-
-    vector<int> a(n);
-    rep(i, n) cin >> a.at(i);
-
-    rep(i, n) cout << a.at(i) << endl;
-
     return 0;
 }
+
