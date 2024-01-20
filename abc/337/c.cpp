2a4dbb288719cd5cbda12e32bde805991431291d
kota <52403688+yashikota@users.noreply.github.com>
2024-01-20 23:37:17 +0900

    update

--- abc/337/c.cpp
+++ abc/337/c.cpp
@@ -3,22 +3,33 @@
 #define ll long long
 
 int main() {
-    int n, tmp;
+    int n;
     std::cin >> n;
 
-    std::vector<int> r;
+    std::vector<int> r(n), s(n);
 
-    for (int i = 0; i < n; i++)
-    {
-        std::cin >> tmp;
-        if (tmp == -1) {
-            r.insert(r.begin(), i + 1);
-        } else {
-            
-        }
+    for (int i = 0; i < n; i++) {
+        std::cin >> r[i];
+    }
+    s = r;
+
+    std::sort(s.begin(), s.end());
+    int t = 0;
+    for (int i = 0; i < n; i++) {
+        if (!(i + 1 == s[i])) t = i + 1;
     }
 
-    std::cout << r[0] << std::endl;
+    s.clear();
+    s.push_back(t);
+    for (int i = 1; i < n; i++) {
+        t = r[t - 1];
+        s.push_back(t);
+    }
+
+    std::reverse(s.begin(), s.end());
+    for (int i = 0; i < n; i++) {
+        std::cout << s[i] << " ";
+    }
 
     return 0;
 }
