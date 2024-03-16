26b32b8ea5041c2bc4bf5261902f5fc35a0ec31b
yashikota <52403688+yashikota@users.noreply.github.com>
2024-03-17 00:04:41 +0900

    update

--- abc/345/a.cpp
+++ abc/345/a.cpp
@@ -6,18 +6,8 @@ int main() {
     std::string s;
     std::cin >> s;
 
-    for(int i = 0; i < s.size(); i++) {
-        if (i == 0) {
-            if (s[i] != '<') break;
-        } else if (i == s.size() - 1) {
-            if (s[i] != '>') break;
-            std::cout << "Yes" << std::endl;
-            return 0;
-        } else {
-            if (s[i] != '=') break;
-        }
-    }
+    std::regex re(R"(<[=]+>)");
+    std::cout << (std::regex_match(s, re) ? "Yes" : "No") << std::endl;
 
-    std::cout << "No" << std::endl;
     return 0;
 }
